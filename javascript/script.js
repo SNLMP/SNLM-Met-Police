467de72ae89edd82ff24e5b99ffa39acba266e90
Tom <tombradley0023@gmail.com>
2026-06-24 05:37:42 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,23 @@
 window.addEventListener("load",()=>{
 
-
 document.body.style.opacity="1";
 
 
-});
+/* STAFF IMAGE SYSTEM */
+
+const staffImages = {
+
+    "M. Rainer": "commissioner.png",
+
+    "C. Murray": "inspector.png",
+
+    "M. Wallace": "inspector.png",
+
+    "T. Walker": "sergeant.png",
+
+    "T. McKenzie": "sergeant.png"
+
+};
 
 
 
@@ -12,25 +25,52 @@ const cards =
 document.querySelectorAll(".card");
 
 
+
 cards.forEach(card=>{
 
+
+let name = card.dataset.name;
+
+
+let image =
+card.querySelector(".profile");
+
+
+
+if(staffImages[name]){
+
+image.src =
+"../assets/staff/" + staffImages[name];
+
+image.alt =
+name;
+
+}
+
+
+
+/* HOVER EFFECT */
 
 card.addEventListener(
 "mouseenter",
 ()=>{
 
-card.style.transform=
+card.style.transform =
 "translateY(-12px)";
 
 });
+
 
 
 card.addEventListener(
 "mouseleave",
 ()=>{
 
-card.style.transform=
+card.style.transform =
 "translateY(0)";
+
+});
+
 
 });
 
